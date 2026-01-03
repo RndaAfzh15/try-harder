975b20ecfdec5a7398bc2139c3b54fcb0a44de9b
Randa alfizah <160241075+RndaAfzh15@users.noreply.github.com>
2026-01-03 16:43:22 +0700

    Update UserList.js

--- Frontend/src/components/UserList.js
+++ Frontend/src/components/UserList.js
@@ -1,33 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import API_URL from "../config";
 
 const UserList = () => {
-const [users, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
 
-useEffect(()=> {
+  useEffect(() => {
     getUsers();
-}, []);
+  }, []);
 
-const getUsers = async () =>{
-        const response = await axios.get("http://localhost:5000/users");
-        setUser(response.data);
-}
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/users`);
+      setUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const deleteUser = async (id) => {
-  try {
-    await axios.delete(`http://localhost:5000/users/${id}`);
-    getUsers();
-  } catch (error) {
-    console.log(error);
-  }
-}
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`${API_URL}/users/${id}`);
+      getUsers();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-        <Link to={`add`} className="button is-success">Add New</Link>
+        <Link to="add" className="button is-success mb-3">Add New</Link>
+
         <table className="table is-striped is-fullwidth">
           <thead>
             <tr>
@@ -39,17 +44,27 @@ const deleteUser = async (id) => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user, index) =>( 
+            {users.map((user, index) => (
               <tr key={user.id}>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.gender}</td>
-              <td>
-                <Link to={`edit/${user.id}`} className="button is-small is-info">Edit</Link>
-                <button  onClick={()=> deleteUser(user.id)}  className="button is-small is-danger">Delete</button>
-              </td>
-            </tr>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <Link
+                    to={`edit/${user.id}`}
+                    className="button is-small is-info mr-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => deleteUser(user.id)}
+                    className="button is-small is-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
