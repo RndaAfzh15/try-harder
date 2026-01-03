e76860ac0b39f6f898d262e5f7ebf95385428347
Randa alfizah <160241075+RndaAfzh15@users.noreply.github.com>
2026-01-03 13:51:21 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import UserRoute from "./routes/UserRoute.js";
+import db from "./config/Database.js";
+
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('Server up and running...'));
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Database connected.");
 
-
-
-
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("DB connection failed:", error);
+  }
+})();
